7a9e661995b529e2b3e7828edf9d4c333714abdb
Jasper Malfetria <56492827+JaZtin21@users.noreply.github.com>
2026-07-11 14:13:38 +0800

    fixed cancel button

--- frontend/src/pages/my-shops/components/ShopForm.tsx
+++ frontend/src/pages/my-shops/components/ShopForm.tsx
@@ -106,7 +106,6 @@ export const ShopForm = ({ data }: { data?: Shop }) => {
         }
     });
 
-    const onCancel = () => { };
     const isLoading = createLoading || isMutationLoading;
 
     // FORM STATE HANDLERS
@@ -470,7 +469,7 @@ export const ShopForm = ({ data }: { data?: Shop }) => {
                     <div className="flex gap-4 justify-end">
                         <button
                             type="button"
-                            onClick={onCancel}
+                            onClick={handleModalClose}
                             disabled={isLoading}
                             className="px-6 py-2 cursor-pointer bg-zinc-200 dark:bg-zinc-700 text-text-main rounded-lg hover:bg-zinc-300 dark:hover:bg-zinc-600 disabled:opacity-50 disabled:cursor-not-allowed transition-colors"
                         >
